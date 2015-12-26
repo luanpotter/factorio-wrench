85a4525e697959e60c52e99f4d39a622b5c70bba
Luan Nico <luanpotter28@hotmail.com>
2015-12-26 21:10:42 -0200

    using only wrench symbol

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -4,12 +4,10 @@ data:extend({{
   icon = "__Wrench__/graphics/icon.png",
   flags = {"placeable-neutral", "not-on-map"},
   collision_mask = { "ghost-layer"},
-  subgroup = "grass",
-  order = "b[decorative]-b[turret-range-overlay-gun]",
   collision_box = {{-0.4, -0.4}, {0.4, 0.4}},
   selection_box = {{-0.5, -0.5}, {0.5, 0.5}},
   selectable_in_game = false,
-  render_layer = "radius-visualization",
+  render_layer = "cursor",
   pictures = {
     {
       filename = "__Wrench__/graphics/icon.png",
