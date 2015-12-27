de617c5043f998b5c170cd4b5f035f5a184e8e2f
Luan Nico <luanpotter28@hotmail.com>
2015-12-27 02:16:18 -0200

    Mispelled wrench subgroup

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -3,7 +3,7 @@ data:extend({{
     name = "wrench",
     icon = "__Wrench__/graphics/icon.png",
     flags = { "goes-to-quickbar" },
-    subgroup = "tools",
+    subgroup = "tool",
     order = "c[automated-construction]-a[blueprint]",
     stack_size = 1,
     stackable = false,
